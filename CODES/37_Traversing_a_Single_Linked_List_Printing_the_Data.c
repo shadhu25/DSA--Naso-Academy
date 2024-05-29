aae969f2f991c2e9d889f853778073179269b796
shadhu25 <shadhushadhu25@gmail.com>
2024-05-29 23:44:50 +0530

    add CODES/37_Traversing_a_Single_Linked_List_Printing_the_Data.c

---
+++ CODES/37_Traversing_a_Single_Linked_List_Printing_the_Data.c
@@ -0,0 +1,35 @@
+// traversal and printing nodes of a single linklist
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+void print(struct node *head){
+    if(head==NULL)
+        printf("Linkedlist is empty!\n");
+    else{
+        struct node *temp=head;
+        printf("[ ");
+        while(temp!=NULL){
+            printf("%d ",temp->data);
+            temp=temp->next;
+        }
+        printf("]\n");
+    }
+}
+int main(){
+    struct node *head=malloc(sizeof(struct node));
+    head->data=45;
+    head->next=NULL;
+    struct node *current=malloc(sizeof(struct node));
+    current->data=46;
+    current->next=NULL;
+    head->next=current;
+    current=malloc(sizeof(struct node));
+    current->data=47;
+    current->next=NULL;
+    head->next->next=current;
+    print(head);
+    return 0;
+}
