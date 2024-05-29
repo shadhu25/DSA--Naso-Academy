d2c533743a48bf43ef9622dc2a29f3b405d27d03
shadhu25 <shadhushadhu25@gmail.com>
2024-05-29 23:44:14 +0530

    add CODES/36_Traversing_a_Single_Linked_List_Counting_the_Nodes.c

---
+++ CODES/36_Traversing_a_Single_Linked_List_Counting_the_Nodes.c
@@ -0,0 +1,38 @@
+// traversal and counting nodes of a single linklist
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+void countNodes(struct node *head){
+    int count=0;
+    if(head==NULL)
+        printf("Linkedlist is empty!\n");
+    else{
+        struct node *temp=head;
+        while(temp!=NULL){
+            count++;
+            temp=temp->next;
+        }
+        printf("total nodes: %d\n",count);
+    }
+}
+int main(){
+    struct node *head=malloc(sizeof(struct node));
+    head->data=45;
+    head->next=NULL;
+    struct node *current=malloc(sizeof(struct node));
+    current->data=46;
+    current->next=NULL;
+    head->next=current;
+    current=malloc(sizeof(struct node));
+    current->data=47;
+    current->next=NULL;
+    head->next->next=current;
+    printf("%d ",head->data);
+    printf("%d ",head->next->data);
+    printf("%d \n",head->next->next->data);
+    countNodes(head);
+    return 0;
+}
