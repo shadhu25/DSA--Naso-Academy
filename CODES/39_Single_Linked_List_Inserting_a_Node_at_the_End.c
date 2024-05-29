aab7b1a94ec564addb412f5a8c63f3c0249549d4
shadhu25 <shadhushadhu25@gmail.com>
2024-05-29 23:45:25 +0530

    add CODES/39_Single_Linked_List_Inserting_a_Node_at_the_End.c

---
+++ CODES/39_Single_Linked_List_Inserting_a_Node_at_the_End.c
@@ -0,0 +1,55 @@
+// adding a new node at end of the linklist
+#include<stdio.h>
+#include<stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+struct node* addAtEnd(struct node *head, int data){
+    struct node *temp, *newNode;
+    newNode=(struct node *)malloc(sizeof(struct node));
+        newNode->data=data;
+        newNode->next=NULL;
+    if(head==NULL){
+        head=newNode;
+        return head;
+    }
+    else{
+        temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newNode;
+        return head;
+    }
+}
+void print(struct node *head){
+    if(head==NULL)
+        printf("Linkedlist is empty!\n");
+    else{
+        struct node *temp=head;
+        printf("[ ");
+        while(temp!=NULL){
+            printf("%d ",temp->data);
+            temp=temp->next;
+        }
+        printf("]\n");
+    }
+}
+int main(){
+    struct node *head=malloc(sizeof(struct node));
+    head->data=45;
+    head->next=NULL;
+    struct node *current=malloc(sizeof(struct node));
+    current->data=46;
+    current->next=NULL;
+    head->next=current;
+    current=malloc(sizeof(struct node));
+    current->data=47;
+    current->next=NULL;
+    head->next->next=current;
+    print(head);
+    head=addAtEnd(head,48);
+    print(head);
+    return 0;
+}
