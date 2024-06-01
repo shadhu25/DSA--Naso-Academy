0287e02d204d8640030fa31688c9d34b365c75df
shadhu25 <shadhushadhu25@gmail.com>
2024-06-01 11:06:33 +0530

    add CODES/Circular_Doubly_LInked_List/93_Circular_Doubly_Linked_List_Deleting_the_First_Node.c

---
+++ CODES/Circular_Doubly_LInked_List/93_Circular_Doubly_Linked_List_Deleting_the_First_Node.c
@@ -0,0 +1,70 @@
+// deleting an element at beg
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    struct node *prev;
+    int data;
+    struct node *next;
+};
+
+struct node* adAtEnd(struct node*,int);
+void print(struct node*);
+struct node* delAtBeg(struct node*);
+
+int main(){
+    struct node *tail=NULL;
+    tail=adAtEnd(tail,45);
+    tail=adAtEnd(tail,47);
+    tail=adAtEnd(tail,48);
+    tail=adAtEnd(tail,49);
+    tail=adAtEnd(tail,50);
+    print(tail);
+    tail=delAtBeg(tail);
+    print(tail);
+    return 0;
+}
+
+struct node* delAtBeg(struct node *tail){
+    if(!tail)
+        return tail;
+    struct node *temp=tail->next;
+    if(temp==tail){
+        free(tail);
+        tail=NULL;
+        return tail;
+    }
+    tail->next=temp->next;
+    temp->next->prev=tail;
+    free(temp);
+    return tail;
+}
+
+void print(struct node *tail){
+    if(!tail)
+        printf("list is empty\n");
+    else{
+        struct node *temp=tail->next;
+        printf("[ ");
+        do{
+            printf("%d ",temp->data);
+            temp=temp->next;
+        }while(temp!=tail->next);
+        printf("]\n");
+    }
+}
+
+struct node* adAtEnd(struct node *tail, int data){
+    struct node *newNode=(struct node *)malloc(sizeof(struct node));
+    newNode->data=data;
+    if(!tail){
+        newNode->next=newNode;
+        newNode->prev=newNode;
+    }
+    else{
+        newNode->prev=tail;
+        newNode->next=tail->next;
+        tail->next=newNode;
+    }
+    return newNode;
+}
