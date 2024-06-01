a12d73b30908caf06f707fd5f3290750b9b48487
shadhu25 <shadhushadhu25@gmail.com>
2024-06-01 09:00:18 +0530

    add CODES/Circular_Singly_LInked_List/88_Circular_Singly_Linked_List_Counting_the_Number_of_Elements.c

---
+++ CODES/Circular_Singly_LInked_List/88_Circular_Singly_Linked_List_Counting_the_Number_of_Elements.c
@@ -0,0 +1,84 @@
+// counting the nodes
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+};
+
+struct node* addToEmpty(int);
+struct node* addAtEnd(struct node*,int);
+void print(struct node*);
+struct node* createCircularSingly(struct node*);
+void nodes(struct node*);
+
+int main(){
+    struct node *tail=NULL;
+    tail=createCircularSingly(tail);
+    print(tail);
+    nodes(tail);
+    return 0;
+}
+
+void nodes(struct node *tail){
+    int count=0;
+    if(!tail)
+        printf("list is empty\n");
+    else{
+        struct node *temp=tail->next;
+        do{
+            count++;
+            temp=temp->next;
+        }while(temp!=tail->next);
+        printf("Total nodes: %d \n",count);
+    }
+}
+
+struct node* createCircularSingly(struct node *tail){
+    int size,data;
+    printf("Please enter the number of nodes: ");
+    scanf("%d",&size);
+    if(size<1)
+        printf("Try again, you entered invalid size(%d)\n",size);
+    else{
+        printf("Enter node1: ");
+        scanf("%d",&data);
+        tail=addToEmpty(data);
+        for(int i=1;i<size;i++){
+            printf("Enter node%d: ",i+1);
+            scanf("%d",&data);
+            tail=addAtEnd(tail,data);
+        }
+    }
+    return tail;
+}
+
+void print(struct node *tail){
+    if(!tail)
+        printf("list is empty\n");
+    else{
+        struct node *temp=tail->next;
+        printf("[ ");
+        do{
+            printf("%d ",temp->data);
+            temp=temp->next;
+        }while(temp!=tail->next);
+        printf("]\n");
+    }
+}
+
+struct node* addToEmpty(int data){
+    struct node *newNode=(struct node *)malloc(sizeof(struct node));
+    newNode->data=data;
+    newNode->next=newNode;
+    return newNode;
+}
+
+struct node* addAtEnd(struct node *tail,int data){
+    struct node *newNode=(struct node *)malloc(sizeof(struct node));
+    newNode->data=data;
+    newNode->next=tail->next;
+    tail->next=newNode;
+    return newNode;
+}
