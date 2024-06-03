ff9f217488c822d60c29cdee987fdf8ed6e659ca
shadhu25 <shadhushadhu25@gmail.com>
2024-06-03 19:27:31 +0530

    add CODES/Stack/Stack_as_Linked_List/119_Linked_List_Implementation_of_Stacks_Part_2.c

---
+++ CODES/Stack/Stack_as_Linked_List/119_Linked_List_Implementation_of_Stacks_Part_2.c
@@ -0,0 +1,44 @@
+// traversal and printing nodes of a single linklist
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+} *top=NULL;
+
+void print();
+void push(int);
+
+int main(){
+    push(55);
+    push(56);
+    push(57);
+    print();
+    return 0;
+}
+
+void push(int data){
+    struct node *newNode;
+    newNode=(struct node *)malloc(sizeof(struct node));
+    if(newNode==NULL){
+        printf("Stack overflow!\n");
+    }
+    else{
+        newNode->data=data;
+        newNode->next=NULL;
+        newNode->next=top;
+        top=newNode;
+    }
+}
+
+void print(){
+        struct node *temp=top;
+        printf("Stack element are:- \n");
+        printf("[ ");
+        while(temp){
+            printf("%d ",temp->data);
+            temp=temp->next;
+        }
+        printf("]\n");
+}
